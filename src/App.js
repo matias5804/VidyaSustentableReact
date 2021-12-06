0516b45f1d3f681e6b073539144b4bd242f4679e
matias5804 <88583185+matias5804@users.noreply.github.com>
2021-12-06 09:36:13 +0100

    Segunda entrega NavBar con ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import ItemListContainer from "./components/ItemListContainer";
+import NavBar from "./components/navBar/NavBar";
+//import Contacto from "./components/pages/Contacto";
+//import Inicio from "./components/pages/Inicio";
+//import Nosotros from "./components/pages/Nosotros";
+//import Tienda from "./components/pages/Tienda";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <ItemListContainer greeting= "HOLA CODERS" />
+
     </div>
   );
 }
